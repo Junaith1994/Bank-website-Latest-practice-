28ecf10a7e6965482b0c3482ef7c8f4ceda45b12
Junaith1994 <daya2018ctg@gmail.com>
2022-11-23 23:43:27 +0600

    added mizanur rahman mizan's information

--- JS/login.js
+++ JS/login.js
@@ -2,7 +2,7 @@ document.getElementById('submit-btn').addEventListener('click', function() {
     const userEmail = document.getElementById('user-email').value;
     const userPass = document.getElementById('user-password').value;
 
-    if((userEmail == "daya2018ctg@gmail.com") && (userPass == "12345") || (userEmail == "Asmaulhusnasonia752004@gmail.com" && (userPass == "asmajunaith"))) {
+    if ((userEmail == "daya2018ctg@gmail.com") && (userPass == "12345") || (userEmail == "Asmaulhusnasonia752004@gmail.com" && (userPass == "asmajunaith")) || (userEmail == "mizan221@gmail.com") && (userPass == "Mizan221#")) {
         window.location.href = "bank-page.html";
     }
     else {alert("Wrong Email Or Password")}
